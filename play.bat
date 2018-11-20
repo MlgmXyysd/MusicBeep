37696b85417222f70231542cf3ca3954b7eff567
Yuze Wu <1204074991@qq.com>
2018-11-20 18:13:21 +0800

    Add files via upload

--- play.bat
+++ play.bat
@@ -3,34 +3,22 @@ title Music Beep Compiler
 set /a err=0
 if /i exist %1.cpp (
 	g++.exe -c %1.cpp -o %1.o -Iinclude -lpthreadGC2
-	if /i "%errorlevel%"=="9009" (
-		set /a err=9009
-	)
-	if /i "%errorlevel%"=="1" (
-		set /a err=1
+	if /i not "%errorlevel%"=="0" (
+		set /a err=%errorlevel%
 	)
 	g++.exe %1.o -o %1.exe -Lib lib/libpthreadGC2.a
-	if /i "%errorlevel%"=="9009" (
-		set /a err=9009
-	)
-	if /i "%errorlevel%"=="1" (
-		set /a err=1
+	if /i not "%errorlevel%"=="0" (
+		set /a err=%errorlevel%
 	)
 	del /f /q %1.o >nul 2>nul
 	if /i exist %1_back.cpp (
 		g++.exe -c %1_back.cpp -o %1_back.o -Iinclude -lpthreadGC2
-		if /i "%errorlevel%"=="9009" (
-			set /a err=9009
-		)
-		if /i "%errorlevel%"=="1" (
-			set /a err=1
+		if /i not "%errorlevel%"=="0" (
+			set /a err=%errorlevel%
 		)
 		g++.exe %1_back.o -o %1_back.exe -Lib lib/libpthreadGC2.a
-		if /i "%errorlevel%"=="9009" (
-			set /a err=9009
-		)
-		if /i "%errorlevel%"=="1" (
-			set /a err=1
+		if /i not "%errorlevel%"=="0" (
+			set /a err=%errorlevel%
 		)
 		del /f /q %1_back.o >nul 2>nul
 	)
