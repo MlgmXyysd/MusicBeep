1e13d653232c82722b5e4a7aa76f2e32df854dc6
mlgmxyysd <1204074991@qq.com>
2019-02-20 00:23:04 +0800

    New music

---
+++ Frog.cpp
@@ -0,0 +1,25 @@
+/*
+* Frog by MlgmXyysd
+* mlgmxyysd991@gmail.com
+* mlgmxyysd@vip.qq.com
+* mlgmxyysd@qq.com
+* Bilibili @mlgmxyysd
+*/
+#include<bits/stdc++.h>
+#include<music.h>
+#define Beat 468.75
+using namespace std;
+int main(int argc,char **argv) {
+	cout<<"Frog by MlgmXyysd."<<endl;
+	cout<<"(01)"<<endl;
+	Beep(B5,Beat/3);
+	Sleep(Beat/1.5);
+	Beep(B6,Beat/3);
+	Sleep(Beat/1.5);
+	Beep(B7,Beat/3);
+	Sleep(Beat/1.5);
+	Beep(B3,Beat/3*2);
+	Beep(B6,Beat/3);
+	cout<<"end."<<endl;
+	return 0;
+}
---
+++ Frog_back.cpp
@@ -0,0 +1,17 @@
+/*
+* Frog(back) by MlgmXyysd
+* mlgmxyysd991@gmail.com
+* mlgmxyysd@vip.qq.com
+* mlgmxyysd@qq.com
+* Bilibili @mlgmxyysd
+*/
+#include<bits/stdc++.h>
+#include<music.h>
+#define Beat 468.75
+using namespace std;
+int main(int argc,char **argv) {
+	cout<<"Frog(back) by MlgmXyysd."<<endl;
+	cout<<"(01)"<<endl;
+	cout<<"end."<<endl;
+	return 0;
+}
